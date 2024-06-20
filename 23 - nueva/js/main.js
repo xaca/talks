dfea51afba38dd56638a34d23cbffb1e4eca4d6c
Xaca Rana <and1res@hotmail.com>
2024-06-20 20:55:28 +0000

    Configuracion de plantilla funcionando

--- 23 - nueva/js/main.js
+++ 23 - nueva/js/main.js
@@ -1,7 +1,3 @@
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-
-let deck = new Reveal({
-  plugins: [Markdown],
-});
-deck.initialize();
+window.onload = ()=>{
+    Reveal.initialize();
+}
